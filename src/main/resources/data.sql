1af2918e670636ceeb560ec0859534058d1fe769
Atanes <aatan@Atanes_Samsung.home>
2018-08-27 22:27:41 -0300

    Atualização dos dados dos usários no arquivo de inicialização DATA.SQL Ajustes no layout da página de lesta de ordens de serviço para acrescentar o campo Defeito do Aparelho

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,17 +1,17 @@
-﻿insert into permissao (id, nome) values 
+insert into permissao (id, nome) values 
 	(1, 'ROLE_OS_ATENDIMENTO')
 	, (2, 'ROLE_OS_ORCAMENTO')
 	, (3, 'ROLE_OS_TECNICO')
 	, (4, 'ROLE_OS_ADMIN')
-;
+    ;
 	
 insert into usuario (id, nome, login, senha, ativo) values 
 	(1, 'Atanes', 'Atanes', '$2a$10$JvyF9q/k/eYwXTVjc4Ay0OT/dCwjW14eT88q3e587jaENTvtt30s2', true)
-	, (2, 'Carlos', 'Carlos', '$2a$10$JvyF9q/k/eYwXTVjc4Ay0OT/dCwjW14eT88q3e587jaENTvtt30s2', true)
-	, (3, 'Técnico', 'Tecnico', '$2a$10$JvyF9q/k/eYwXTVjc4Ay0OT/dCwjW14eT88q3e587jaENTvtt30s2', true)
+	, (2, 'Carlos', 'carlos', '$2a$10$JvyF9q/k/eYwXTVjc4Ay0OT/dCwjW14eT88q3e587jaENTvtt30s2', true)
+	, (3, 'Técnico', 'tecnico', '$2a$10$JvyF9q/k/eYwXTVjc4Ay0OT/dCwjW14eT88q3e587jaENTvtt30s2', true)
 	, (4, 'Administrador', 'admin', '$2a$10$JvyF9q/k/eYwXTVjc4Ay0OT/dCwjW14eT88q3e587jaENTvtt30s2', true)
-	, (5, 'Atendente', 'Atendente', '$2a$10$JvyF9q/k/eYwXTVjc4Ay0OT/dCwjW14eT88q3e587jaENTvtt30s2', true)
-	, (6, 'Orcamento', 'Orcamento', '$2a$10$JvyF9q/k/eYwXTVjc4Ay0OT/dCwjW14eT88q3e587jaENTvtt30s2', true)
+	, (5, 'Atendente', 'atendente', '$2a$10$JvyF9q/k/eYwXTVjc4Ay0OT/dCwjW14eT88q3e587jaENTvtt30s2', true)
+	, (6, 'Orcamento', 'orcamento', '$2a$10$JvyF9q/k/eYwXTVjc4Ay0OT/dCwjW14eT88q3e587jaENTvtt30s2', true)
 ;
 	
 insert into usuario_permissao (usuario_id, permissao_id) values 
@@ -22,30 +22,3 @@ insert into usuario_permissao (usuario_id, permissao_id) values
 	, (5, 1)
 	, (6, 2)
 ;
-
-
-
-
-
-
-
-
-
-
---insert into grupo (id, nome) values 
---	(1, 'DIRETOR') 
---	, (2, 'GERENTE')
---;
-	
---insert into usuario_grupo (usuario_id, grupo_id) values 
---	(1, 1)
---	, (2, 2)
---;
-	
---insert into grupo_permissao (grupo_id, permissao_id) values 
---	(1, 1)
---	, (1, 2)
---	, (1, 3)
---	, (2, 1)
---	, (2, 2)
---;
