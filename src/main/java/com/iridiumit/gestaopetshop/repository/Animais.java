ebc0f132b65e5ba87696fd306bc621b190060f83
alexandre.atanes <alexandre.atanes@SOU-042.sou.local>
2019-07-25 16:05:42 -0300

    Incusão de um método para pesquisar os animais por raça.

--- src/main/java/com/iridiumit/gestaopetshop/repository/Animais.java
+++ src/main/java/com/iridiumit/gestaopetshop/repository/Animais.java
@@ -14,5 +14,7 @@ public interface Animais extends JpaRepository<Animal, Long>{
 	List<Animal> findByCliente (Cliente c);
 
 	List<Animal> findByNomeContainingIgnoreCase(String nome);
+	
+	List<Animal> findByRaca(String raca);
 
 }
